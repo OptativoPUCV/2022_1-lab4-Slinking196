3b25935462be6601348318b6e8ad43ab74533abe
Slinking196 <matias.bugueno.b@mail.pucv,cl>
2022-04-09 00:55:30 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -109,7 +109,7 @@ Pair * nextMap(HashMap * map) {
     size_t i = (map->current + 1) % map->capacity;
 
     if (map->size == 0) return NULL;
-    while (map->buckets[i] == NULL && map->buckets[i]->key == NULL) {
+    while (map->buckets[i] == NULL) {
         i = (i + 1) % map->capacity;
     }
 
