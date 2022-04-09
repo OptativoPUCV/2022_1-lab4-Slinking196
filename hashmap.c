f183c80a688f670da5db931ac436c71b1a101f5b
Slinking196 <matias.bugueno.b@mail.pucv,cl>
2022-04-09 01:26:29 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -41,6 +41,11 @@ int is_equal(void* key1, void* key2){
 void insertMap(HashMap * map, char * key, void * value) {
     Pair *new = createPair(key, value);
     long i = hash(key, map->capacity);
+    float porcentaje = map->size / map->capacity;
+
+    if (porcentaje > 0.7) {
+        enlarge(map);
+    }
 
     while (map->buckets[i] != NULL && map->buckets[i]->key != NULL)  {
         i = (i + 1) % map->capacity;
