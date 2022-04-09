f35b8c642f19c68c74b88e071d02413ac1c44e63
Slinking196 <matias.bugueno.b@mail.pucv,cl>
2022-04-08 22:59:40 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -49,9 +49,11 @@ bool esIgual(void *key, void *key2) {
 void insertMap(HashMap * map, char * key, void * value) {
     Pair *new = createPair(key, value);
     long i = hash(key, map->capacity);
+    void *auxKey = map->buckets[i]->key;
 
-    while (esIgual(key, map->buckets[i]->key)) {
+    while (esIgual(key, auxKey)) {
         i++;
+        auxKey = map->buckets[i]->key;
         if (i >= map->capacity) break;;
     }
     map->buckets[i] = new;
