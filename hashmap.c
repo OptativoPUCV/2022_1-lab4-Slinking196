c6439d2ef0fce6db40fbfa31573d8c996e538940
Slinking196 <matias.bugueno.b@mail.pucv,cl>
2022-04-09 01:21:27 -0400

    new try

--- hashmap.c
+++ hashmap.c
@@ -54,7 +54,7 @@ void enlarge(HashMap * map) {
     enlarge_called = 1; //no borrar (testing purposes)
     Pair **oldBuckets = map->buckets;
     size_t tamano = map->capacity;
-    size_t i, j;
+    size_t i;
 
     map->capacity *= 2;
     map->buckets = (Pair **) calloc(map->capacity, sizeof(Pair *));
